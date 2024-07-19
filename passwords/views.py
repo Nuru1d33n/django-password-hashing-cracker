699f1c282d12e8b6bac07e26ecbacdbdd2df1160
Nurudeen Adebileje <Nuruadebileje@gmail.com>
2024-07-19 04:59:13 -0400

    Initial commit with Django project setup

--- passwords/views.py
+++ passwords/views.py
@@ -1,13 +1,10 @@
 import hashlib
-from django.shortcuts import render
-from passwords.forms import PasswordForm, HashForm
+from django.shortcuts import render, redirect
+from passwords.forms import PasswordForm, HashForm, HashCheckForm, IdentifyHashForm
 from passwords.models import (
     Password, MD5Password, SHA1Password, SHA224Password,
     SHA256Password, SHA384Password, SHA512Password, SHA3256Password
 )
-from passwords.utils import identify_hash_type
-
-
 
 HASH_TYPES = {
     'md5': 32,
@@ -26,15 +23,13 @@ def get_hash_type(hash_value):
             return hash_type
     return None
 
-
 def home(request):
     passwords = []
-    hash_type_result = None
+    error_message = None
 
     if request.method == 'POST':
         form = PasswordForm(request.POST, request.FILES)
         hash_form = HashForm(request.POST)
-        hash_check_form = HashCheckForm(request.POST)
 
         if form.is_valid():
             choice = form.cleaned_data['choice']
@@ -43,9 +38,46 @@ def home(request):
             password_file = request.FILES.get('password_file')
 
             def save_password(password):
-                if not Password.objects.filter(value=password).exists():
-                    Password.objects.create(value=password)
+                # Check if password exists in Password table
+                if Password.objects.filter(value=password).exists():
+                    nonlocal error_message
+                    error_message = f"The password '{password}' already exists."
+                else:
+                    # Save password to Password table if it does not exist
+                    password_instance = Password.objects.create(value=password)
                     passwords.append(password)
+                    # Hash the password and save it in various hash type tables
+                    hash_and_save(password, password_instance)
+
+            def hash_and_save(password, password_instance):
+                hash_functions = {
+                    'md5': hashlib.md5,
+                    'sha1': hashlib.sha1,
+                    'sha224': hashlib.sha224,
+                    'sha256': hashlib.sha256,
+                    'sha384': hashlib.sha384,
+                    'sha512': hashlib.sha512,
+                    'sha3_256': hashlib.sha3_256,
+                }
+
+                for hash_type, hash_func in hash_functions.items():
+                    hashed_password = hash_func(password.encode()).hexdigest()
+                    save_hashed_password(hash_type, hashed_password, password_instance)
+
+            def save_hashed_password(hash_type, hashed_value, original_password):
+                models = {
+                    'md5': MD5Password,
+                    'sha1': SHA1Password,
+                    'sha224': SHA224Password,
+                    'sha256': SHA256Password,
+                    'sha384': SHA384Password,
+                    'sha512': SHA512Password,
+                    'sha3_256': SHA3256Password,
+                }
+                model = models.get(hash_type)
+                if model:
+                    if not model.objects.filter(value=hashed_value).exists():
+                        model.objects.create(value=hashed_value, original_password=original_password)
 
             if choice == 'input' and password_input:
                 save_password(password_input)
@@ -59,42 +91,8 @@ def home(request):
                     save_password(line.decode().strip())
 
         if hash_form.is_valid():
-            hash_type = hash_form.cleaned_data['hash_type']
-            print(hash_type)
-            password = hash_form.cleaned_data['password']
-
-            def save_hashed_password(model, hashed_value):
-                if not model.objects.filter(value=hashed_value).exists():
-                    model.objects.create(value=hashed_value)
-
-            hashed_password = None
-            if hash_type == 'md5':
-                hashed_password = hashlib.md5(password.encode()).hexdigest()
-                save_hashed_password(MD5Password, hashed_password)
-
-            elif hash_type == 'sha1':
-                hashed_password = hashlib.sha1(password.encode()).hexdigest()
-                save_hashed_password(SHA1Password, hashed_password)
-
-            elif hash_type == 'sha224':
-                hashed_password = hashlib.sha224(password.encode()).hexdigest()
-                save_hashed_password(SHA224Password, hashed_password)
-
-            elif hash_type == 'sha256':
-                hashed_password = hashlib.sha256(password.encode()).hexdigest()
-                save_hashed_password(SHA256Password, hashed_password)
-
-            elif hash_type == 'sha384':
-                hashed_password = hashlib.sha384(password.encode()).hexdigest()
-                save_hashed_password(SHA384Password, hashed_password)
-
-            elif hash_type == 'sha512':
-                hashed_password = hashlib.sha512(password.encode()).hexdigest()
-                save_hashed_password(SHA512Password, hashed_password)
-
-            elif hash_type == 'sha3_256':
-                hashed_password = hashlib.sha3_256(password.encode()).hexdigest()
-                save_hashed_password(SHA3256Password, hashed_password)
+            # Handle hash form if needed
+            pass
 
     else:
         form = PasswordForm()
@@ -104,109 +102,45 @@ def home(request):
         'form': form,
         'hash_form': hash_form,
         'passwords': passwords,
+        'error_message': error_message,
     }
     return render(request, 'home/index.html', context)
 
-
-import hashlib
-from django.shortcuts import render, redirect
-from passwords.models import (
-    Password, MD5Password, SHA1Password, SHA224Password,
-    SHA256Password, SHA384Password, SHA512Password, SHA3256Password
-)
-
 def convert_passwords(request):
     if request.method == 'POST':
-        # Get all passwords from the Password table
         passwords = Password.objects.all()
         
-        def save_hashed_password(model, hashed_value):
+        def save_hashed_password(model, hashed_value, original_password):
             if not model.objects.filter(value=hashed_value).exists():
-                model.objects.create(value=hashed_value)
+                model.objects.create(value=hashed_value, original_password=original_password)
         
-        # Iterate over each password and save hashed versions
         for password in passwords:
             pwd_value = password.value
             
-            # MD5
             md5_hashed = hashlib.md5(pwd_value.encode()).hexdigest()
-            save_hashed_password(MD5Password, md5_hashed)
+            save_hashed_password(MD5Password, md5_hashed, password)
 
-            # SHA1
             sha1_hashed = hashlib.sha1(pwd_value.encode()).hexdigest()
-            save_hashed_password(SHA1Password, sha1_hashed)
+            save_hashed_password(SHA1Password, sha1_hashed, password)
 
-            # SHA224
             sha224_hashed = hashlib.sha224(pwd_value.encode()).hexdigest()
-            save_hashed_password(SHA224Password, sha224_hashed)
+            save_hashed_password(SHA224Password, sha224_hashed, password)
 
-            # SHA256
             sha256_hashed = hashlib.sha256(pwd_value.encode()).hexdigest()
-            save_hashed_password(SHA256Password, sha256_hashed)
+            save_hashed_password(SHA256Password, sha256_hashed, password)
 
-            # SHA384
             sha384_hashed = hashlib.sha384(pwd_value.encode()).hexdigest()
-            save_hashed_password(SHA384Password, sha384_hashed)
+            save_hashed_password(SHA384Password, sha384_hashed, password)
 
-            # SHA512
             sha512_hashed = hashlib.sha512(pwd_value.encode()).hexdigest()
-            save_hashed_password(SHA512Password, sha512_hashed)
+            save_hashed_password(SHA512Password, sha512_hashed, password)
 
-            # SHA3-256
             sha3_256_hashed = hashlib.sha3_256(pwd_value.encode()).hexdigest()
-            save_hashed_password(SHA3256Password, sha3_256_hashed)
+            save_hashed_password(SHA3256Password, sha3_256_hashed, password)
         
         return redirect('home')
 
     return render(request, 'home/convert.html')
-
-
-
-# import hashlib
-# from django.shortcuts import render
-# from .forms import HashIdentifyForm
-
-# def identify_hash(request):
-#     hash_type = None
-#     hash_value = None
-
-#     if request.method == 'POST':
-#         form = HashIdentifyForm(request.POST)
-#         if form.is_valid():
-#             hash_value = form.cleaned_data['hash_value']
-#             known_password = 'knownpassword'
-            
-#             hash_algorithms = {
-#                 'md5': hashlib.md5(known_password.encode()).hexdigest(),
-#                 'sha1': hashlib.sha1(known_password.encode()).hexdigest(),
-#                 'sha224': hashlib.sha224(known_password.encode()).hexdigest(),
-#                 'sha256': hashlib.sha256(known_password.encode()).hexdigest(),
-#                 'sha384': hashlib.sha384(known_password.encode()).hexdigest(),
-#                 'sha512': hashlib.sha512(known_password.encode()).hexdigest(),
-#                 'sha3_256': hashlib.sha3_256(known_password.encode()).hexdigest()
-#             }
-
-#             for algo, hashed in hash_algorithms.items():
-#                 if hash_value == hashed:
-#                     hash_type = algo
-#                     break
-
-#     else:
-#         form = HashIdentifyForm()
-
-#     context = {
-#         'form': form,
-#         'hash_type': hash_type,
-#         'hash_value': hash_value
-#     }
-#     return render(request, 'home/identify_hash.html', context)
-
-
-import hashlib
-from django.shortcuts import render
-from passwords.forms import IdentifyHashForm
-from passwords.models import (MD5Password, SHA1Password, SHA224Password, SHA256Password,
-                              SHA384Password, SHA512Password, SHA3256Password)
 
 def identify_hash(request):
     hash_value = None
@@ -265,26 +199,3 @@ def identify_hash(request):
         'hash_value': hash_value,
     }
     return render(request, 'home/identify_hash.html', context)
-
-
-def identify_hash_type(hash_value):
-    hash_length = len(hash_value)
-    
-    if hash_length == 32:
-        return 'MD5'
-    elif hash_length == 40:
-        return 'SHA1'
-    elif hash_length == 56:
-        return 'SHA224'
-    elif hash_length == 64:
-        return 'SHA256'
-    elif hash_length == 96:
-        return 'SHA384'
-    elif hash_length == 128:
-        return 'SHA512'
-    elif hash_length == 64 and all(c in '0123456789abcdefABCDEF' for c in hash_value):
-        return 'SHA3-256'
-    else:
-        return 'Unknown hash type'
-
-
